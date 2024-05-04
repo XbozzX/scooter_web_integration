3d105b2cba19ab718cf0826147048e4dfdcd847d
faizhafiii <ahmad_19000636@utp.edu.my>
2024-05-05 00:51:04 +0800

    tambah page aboutUs.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import BookingPage from "./pages/bookingPage";
 import Scheduler_sc1_Page from "./pages/scheduler_sc1_Page";
 import Scheduler_sc2_Page from "./pages/scheduler_sc2_Page";
+import AboutUs from "./pages/aboutUs";
 
 function App() {
   return (
     // PLEASE INSERT THE PATH FOR THE PAGES HERE...
     // DO NOT INSERT ANY CODE THAT NOT PATH PAGES
     <Routes>
-      <Route path="/" element={<BookingPage />} />
+      <Route path="/" element={<AboutUs />} />
       <Route path="/Bookings/scheduler_1" element={<Scheduler_sc1_Page />} />
       <Route path="/Bookings/scheduler_2" element={<Scheduler_sc2_Page />} />
     </Routes>
